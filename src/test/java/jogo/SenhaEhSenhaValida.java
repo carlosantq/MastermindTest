88b9880082613841c5fbdd2f194ba0dd491960af
Jonathan <Jonathan@192.168.1.25>
2017-05-29 23:13:08 -0300

    Começo do ehSenhaValida()

---
+++ src/test/java/jogo/SenhaEhSenhaValida.java
@@ -0,0 +1,20 @@
+package jogo;
+
+import org.junit.*;
+
+/*
+ * Classe de teste do método ehSenhaValida() da classe Senha.
+ * 
+ * Por ser impossível testar todas as possibilidades de senhas válidas e 
+ * inválidas optamos por...........
+ */
+
+public class SenhaEhSenhaValida {
+
+	private Senha senha;
+	
+	@Test
+	public void senhaInvalidaVermelho() {
+		
+	}
+}
